fc74ff1c5789b89ac86b54040d5cdb3492e5a4e1
IanaPostolachi <304119@via.dk>
2022-08-15 11:08:42 +0200

    controller changed

--- src/meetups/meeups.controller.ts
+++ src/meetups/meeups.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { MeetupsServices } from './meetups.service';
 
 @Controller()
 export class MeetupsController {
   constructor(private readonly MeetupsServices: MeetupsServices) {}
+
+  @Get()
+  getAllMeetups(){
+    
+  }
 }
